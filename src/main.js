460bfa6b2065ab33be86b4aa6fcfb2104c7bdb07
Noel Rosales <wb-noelrosal625093@antfin.com>
2020-02-26 16:06:50 +0800

    removed css theme package

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import * as firebase from 'firebase/app'
 import firebaseConfig from './firebaseConfig'
-import 'test-css-theme/build/dist/theme.min.css'
 
 firebase.initializeApp(firebaseConfig)
 
