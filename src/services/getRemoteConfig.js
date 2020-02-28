50cb02a0eba7bcef65f08ed3f04f7c731c942f59
Noel Rosales <wb-noelrosal625093@antfin.com>
2020-02-28 17:48:49 +0800

    delete indexedDb on load

--- src/services/getRemoteConfig.js
+++ src/services/getRemoteConfig.js
@@ -2,6 +2,7 @@ import * as firebase from 'firebase/app'
 import 'firebase/remote-config'
 
 export default async function (config) {
+  indexedDB.deleteDatabase('firebase_remote_config')
   const remoteConfig = firebase.remoteConfig()
 
   await remoteConfig.fetchAndActivate()
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,6 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'home',
   async beforeMount () {
-    indexedDB.deleteDatabase('firebase_remote_config')
     await this.LOAD_CONFIG()
   },
   methods: {
